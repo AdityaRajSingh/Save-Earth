33f2b28b51342bae7c9c147daa62d6b7a0ff45bb
Adnan Azmat <adnanazmat.is.cool@gmail.com>
2018-10-12 10:05:58 +0530

    Updated a_p_t.py

--- accessing_published_tweets.py
+++ accessing_published_tweets.py
@@ -93,6 +93,11 @@ class TweetAnalyzer:
     def tweets_to_data_frame(self, tweets):
         df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
         df['id'] = np.array([tweet.id for tweet in tweets])
+        df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        df['date'] = np.array([tweet.created_at for tweet in tweets])
+        df['source'] = np.array([tweet.source for tweet in tweets])
+        df['likes'] = np.array([tweet.favourite_count for tweet in tweets])
+        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
 
         return df
 
