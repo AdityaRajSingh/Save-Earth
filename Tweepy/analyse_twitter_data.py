2a23addd250de26bd4120b96685c71a282b7dfec
Adnan Azmat <adnanazmat.is.cool@gmail.com>
2018-10-18 16:49:20 +0530

    Added sms feature

--- Tweepy/analyse_twitter_data.py
+++ Tweepy/analyse_twitter_data.py
@@ -9,7 +9,8 @@ import re
 # import twitter_credentials
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+# plt.use('TkAgg')
 import json
 import time
 
