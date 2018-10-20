5594a7c0a027bc7e8dce0dc5eba8efbcc246714e
amntoppo <amntoppo@gmail.com>
2018-10-20 14:47:55 +0530

    Initial Commit

---
+++ Android/save_earth/app/src/main/java/com/example/save_earth/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.save_earth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by root on 20/10/18.
+ */
+
+public class MainActivity extends Activity {
+
+    public void onCreate(Bundle savedInstance) {
+
+        super.onCreate(savedInstance);
+    }
+}
